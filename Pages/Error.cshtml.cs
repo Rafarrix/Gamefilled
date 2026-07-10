dcbf89113ac3e46976e429fc5ca6347afcfd8a71
Rafarrix <rafarrixbruno2000@gmail.com>
2026-07-10 23:05:33 +0100

    feat: add user-friendly status error model

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -1,28 +1,37 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
 
-namespace Gamefilled.Pages
+namespace Gamefilled.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public sealed class ErrorModel : PageModel
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public string? RequestId { get; private set; }
+    public bool ShowRequestId => !string.IsNullOrWhiteSpace(RequestId);
+
+    [BindProperty(SupportsGet = true)]
+    public int? StatusCode { get; set; }
+
+    public string Heading => StatusCode switch
     {
-        public string? RequestId { get; set; }
+        404 => "Page not found",
+        403 => "Access denied",
+        _ => "Something went wrong"
+    };
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public string Message => StatusCode switch
+    {
+        404 => "The page may have moved, the link may be outdated or the address may be incomplete.",
+        403 => "You do not have permission to open this page.",
+        _ => "Gamefilled could not complete this request. Try again or return to discovery."
+    };
 
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+    public void OnGet(int? statusCode = null)
+    {
+        StatusCode = statusCode ?? StatusCode ?? 500;
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        Response.StatusCode = StatusCode.Value;
     }
-
 }
